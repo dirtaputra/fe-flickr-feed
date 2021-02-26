3d5349ce47f66cd417696edbc656b9b22040c356
Dirta Putra Anggara <dirtaputraanggara@gmail.com>
2021-02-26 22:05:42 +0700

    [ADD] fetch data

--- pages/index.vue
+++ pages/index.vue
@@ -2,64 +2,32 @@
   <div>
     <nav class="navbar navbar-light sticky-top bg-light">
       <a class="navbar-brand" href="#">Flickr - Feed</a>
-      <ul class="nav">
-        <li class="nav-item">
-          <button
-            type="button"
-            class="btn btn-primary btn-md btn-success"
-            data-toggle="modal"
-            data-target="#exampleModal"
-          >Refresh</button>
-        </li>
-      </ul>
     </nav>
 
     <div class="container-fluid">
-      <!-- Nav pills -->
-      <ul class="nav nav-pills justify-content-center mt-4" role="tablist">
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="pill" href="#home">
-            <i class="fa fa-square" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="pill" href="#menu2">
-            <i class="fa fa-th" aria-hidden="true"></i>
-          </a>
-        </li>
-      </ul>
-
       <!-- Tab panes -->
       <div class="tab-content">
         <div id="home" class="container-fluid tab-pane active">
           <br />
           <div class="row justify-content-center">
             <div class>
-              <div class="card mx-auto custom-card" id="prova">
+              <div class="card mx-auto custom-card" id="prova" v-for="feed of feed" :key="feed">
                 <div class="row post-header col-12 py-2 px-3">
                   <div class="col-6 float-left">
-                    <h4>My first post</h4>
+                    <h4>{{feed.title}}</h4>
                   </div>
                   <div class="col-6 float-right text-right post-number">
-                    <h4>12/14</h4>
+                    <h4>{{feed.author}}</h4>
                   </div>
                 </div>
-                <img
-                  class="card-img"
-                  src="https://assets.breatheco.de/apis/img/images.php?blob&tags=bobdylan"
-                  alt="Card image cap"
-                />
+                <img class="card-img" :src="feed.pict" width="700 px" />
                 <div class="card-body px-3">
-                  <h5 class="card-title">1000 Likes</h5>
-                  <p
-                    class="card-text"
-                  >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <h5 class="card-title">Date: {{feed.published}}</h5>
                 </div>
                 <div class="row post-header px-3 pb-3">
                   <div class="col-1 float-left text-left">
                     <i class="far fa-heart"></i>
                   </div>
-                  <div class="col-10 float-left text-left">Comment...</div>
                   <div class="col-1 float-right text-right">
                     <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
                   </div>
@@ -74,5 +42,16 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      feed: []
+    };
+  },
+  async fetch() {
+    this.feed = await fetch("http://localhost:3001/getFeed").then(res =>
+      res.json()
+    );
+  }
+};
 </script>
